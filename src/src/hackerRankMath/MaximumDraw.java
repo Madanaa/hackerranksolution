95f5a3ba9ec82fac0dba5cd06926861639d57c5b
Madan Gangwar <madanji.official@gmail.com>
2022-07-23 13:30:27 +0530

    added

--- src/src/hackerRankMath/MaximumDraw.java
+++ src/src/hackerRankMath/MaximumDraw.java
@@ -1,5 +1,12 @@
 package hackerRankMath;
 
 public class MaximumDraw {
+    static int maxDraw(int n) {
+        return n+1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxDraw(2));
+    }
 
 }
