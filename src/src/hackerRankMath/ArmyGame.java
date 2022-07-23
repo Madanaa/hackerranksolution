59e9a6cc40ac603181bcef6d17bd6ac2e01a5de1
Madan Gangwar <madanji.official@gmail.com>
2022-07-23 23:45:49 +0530

    added

--- src/src/hackerRankMath/ArmyGame.java
+++ src/src/hackerRankMath/ArmyGame.java
@@ -5,16 +5,18 @@ import java.util.Scanner;
 //army game hackerrank solution in java
 //Method 1
 public class ArmyGame {
-    static int gameCell(int n, int m) {
-        int result = (n/2 + n%2)*(m/2 + m%2);
-        return result;
-    }
-
+//    static int gameCell(int n, int m) {
+//        int result = (n/2 + n%2)*(m/2 + m%2);
+//        return result;
+//    }
+// method 2
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        System.out.println(gameCell(n,m));
+        int result = ((n+1)/2 *((m+1)/2));
+        System.out.println(result);
+
     }
 
 }
